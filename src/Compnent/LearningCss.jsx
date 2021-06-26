7b0849666d5679e86bda9b1427b06db4a3fcdb24
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-06-26 13:43:12 +0530

    Heading

--- src/Compnent/LearningCss.jsx
+++ src/Compnent/LearningCss.jsx
@@ -4,7 +4,7 @@ export const LearningCss = () =>{
      return(
          <div>
              <p>
-                 
+                <h1>Paragraph1</h1>
                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptatem.
              </p>
          </div>
