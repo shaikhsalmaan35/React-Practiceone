b7eb5a3364c86289599632294a7fc26d4bbec272
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-06-29 20:01:28 +0530

    import-UseState-hook

--- src/Compnent/Landing/Landing.jsx
+++ src/Compnent/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { LearningCss } from '../Learning/LearningCss';
-
+import { UseStateHook } from '../Hooks/UseStateHooks';
 export const Landing = () => {
     return (
         <div>
             <LearningCss/>
+            <UseStateHook/>
+
         </div>
     );
 };
